fedfc512ac5cc69a51e65f4ea91a34c4079179c2
saadasif04 <asifnawazmalik75@gmai.com@example.com>
2026-05-10 22:15:19 +0500

    Cors Issue Fixed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,7 +15,11 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://multi-tenant-crm-kn3j.vercel.app', // your Vercel URL
+    ],
     credentials: true,
   });
 
